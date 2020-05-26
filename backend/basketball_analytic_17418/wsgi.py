a01d751c24eb6f7690ae3ddc5f759e4e5da8792f
Crowdbotics <team@crowdbotics.com>
2020-05-26 17:07:42 +0000

    Update models

--- backend/basketball_analytic_17418/wsgi.py
+++ backend/basketball_analytic_17418/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'basketball_analytic_17418.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "basketball_analytic_17418.settings")
 
 application = get_wsgi_application()
